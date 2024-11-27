615d7264408f76493739b13d32eb158aecd894f6
QKData <quangkien.md@gmail.com>
2024-11-27 16:10:31 +0700

    Modify sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0
+    if (start < 0 | end < 0 | !Number.isInteger(start) | !Number.isInteger(end)) {
+        return "ERROR"
+    }
+    else if (start > end) {
+        for (let i = end; i <= start; i++) {
+            sum = sum + i;
+        }
+        return sum;
+    }
+    else {
+        for (let i = start; i <= end; i++) {
+            sum = sum + i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line

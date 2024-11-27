53b7aeddad3047df5f2c7ffa0a2bd71654ed7e70
QKData <quangkien.md@gmail.com>
2024-11-27 13:43:45 +0700

    Modify repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(str, nums) {
+    if (nums < 0)
+        return "ERROR"
+    else
+        return str.repeat(nums);
 };
 
 // Do not edit below this line

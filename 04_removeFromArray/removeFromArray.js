73314f0f5e79eb5c712f1802d8c76254741f1965
QKData <quangkien.md@gmail.com>
2024-11-27 15:01:15 +0700

    Modify removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elementsToRemove) {
+    return arr.filter(item => !elementsToRemove.includes(item));
 };
 
 // Do not edit below this line

a93bb0ecb53abb2d35f1f071cc2fc3f6819866aa
QKData <quangkien.md@gmail.com>
2024-11-27 14:11:58 +0700

    Modify reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    newStr = str.split("");
+    revStr = ""
+    for (let i = str.length - 1; i >= 0; i--) {
+        revStr = revStr.concat(newStr[i]);
+    }
+    return revStr;
 };
 
 // Do not edit below this line
